835e7c923af910c5ae8ae25e29a1e2c994d8e2d6
Ralph McArdell <ralphmcardell@gmail.com>
2017-08-31 11:22:59 +0100

    Changed Panic to Error in names, removed to which code panics related comments, make error message strings less specific

--- cxx11_custom_error_code_example.cpp
+++ cxx11_custom_error_code_example.cpp
@@ -3,17 +3,16 @@
 // In game/renderer_panic.hpp
 namespace TheGame
 {
-  enum class RendererPanic
-  {
-    GameDimensionTooSmall = 100 // Panics raised by GetSupportedGameDimensions
+  enum class RendererError
+  { GameDimensionTooSmall = 100
   , GameDimensionBadRange
-  , BoardTooSmall         = 200 // Panics raised by GetSupportedBoardSizes
+  , BoardTooSmall         = 200
   , BoardBadRange
   , GameDimensionBad
-  , BoardNotSquare        = 300 // Panics raised by NewGameRenderer
+  , BoardNotSquare        = 300
   , BoardSizeBad
-  , BadRegion             = 400 // Panics raised by DrawBoard
-  , CellCoordinateBad     = 500 // Panics raised by DrawCell
+  , BadRegion             = 400
+  , CellCoordinateBad     = 500
   , NewStateInvalid
   , PrevStateInvalid
   };
@@ -22,14 +21,13 @@ namespace TheGame
 // In game/appengine_panic.hpp
 namespace TheGame
 {
-  enum class AppEnginePanic
-  {
-    NoObjectConIx       = 100 // Panics raised by AppSession
+  enum class AppEngineError
+  { NoObjectConIx       = 100
   , NoRendererContainer
-  , NullDrawAction      = 200 // Panics raised by GameSession
-  , BadDrawContext      = 300 // Panics raised by DrawAction or specialisations
+  , NullDrawAction      = 200
+  , BadDrawContext      = 300
   , BadGameObject
-  , NullPlayer          = 400 // Panics raised by GameSession
+  , NullPlayer          = 400
   };
 }
 
@@ -39,19 +37,19 @@ namespace TheGame
 #include <system_error>
 namespace std
 {
-  using TheGame::RendererPanic;
-  template <> struct is_error_code_enum<RendererPanic> : true_type {};
-  using TheGame::AppEnginePanic;
-  template <> struct is_error_code_enum<AppEnginePanic> : true_type {};
+  using TheGame::RendererError;
+  template <> struct is_error_code_enum<RendererError> : true_type {};
+  using TheGame::AppEngineError;
+  template <> struct is_error_code_enum<AppEngineError> : true_type {};
 }
 
 namespace TheGame
 {
-  enum class RendererPanic;
-  std::error_code make_error_code(RendererPanic);
+  enum class RendererError;
+  std::error_code make_error_code(RendererError);
 
-  enum class AppEnginePanic;
-  std::error_code make_error_code(AppEnginePanic);
+  enum class AppEngineError;
+  std::error_code make_error_code(AppEngineError);
 }
 
 // In game/error_codes.cpp
@@ -59,97 +57,97 @@ namespace TheGame
 
 namespace
 {
-  struct RendererPanicCategory : std::error_category
+  struct RendererErrorCategory : std::error_category
   {
     const char* name() const noexcept override;
     std::string message(int ev) const override;
   };
 
-  const char* RendererPanicCategory::name() const noexcept 
+  const char* RendererErrorCategory::name() const noexcept 
   {
     return "renderer";
   }
 
-  std::string RendererPanicCategory::message( int ev ) const
+  std::string RendererErrorCategory::message( int ev ) const
   {
-    using TheGame::RendererPanic;
-    switch( static_cast<RendererPanic>(ev) )
+    using TheGame::RendererError;
+    switch( static_cast<RendererError>(ev) )
     {
-    case RendererPanic::GameDimensionTooSmall:
-      return "Renderer reported min. supported game grid less than 3 x 3";
-    case RendererPanic::GameDimensionBadRange:
-      return "Render reported a max. supported game grid less than the min.";
-    case RendererPanic::BoardTooSmall:
-      return "Renderer reported a min. supported pixel length less than 1";
-    case RendererPanic::BoardBadRange:
-      return "Render reported a max. supported pixel length less than the min.";
-    case RendererPanic::GameDimensionBad:
-      return "Game dimension passed to GetSupportedBoardSizes is not supported";
-    case RendererPanic::BoardNotSquare:
-      return "Game rect. passed to NewGameRenderer must be square";
-    case RendererPanic::BoardSizeBad:
-      return "Game rect. passed to NewGameRenderer not supported but this renderer";
-    case RendererPanic::BadRegion:
-      return "DrawBoard only accepts positive height and width values.";
-    case RendererPanic::CellCoordinateBad:
-      return "DrawCell only accepts positive cell X and Y index values.";
-    case RendererPanic::NewStateInvalid:
-      return "DrawCell: passed in requested new state is invalid";
-    case RendererPanic::PrevStateInvalid:
-      return "DrawCell: passed in value of previous state is invalid";
+    case RendererError::GameDimensionTooSmall:
+      return "Reported min. supported game grid less than 3 x 3";
+    case RendererError::GameDimensionBadRange:
+      return "Reported a max. supported game grid less than the min.";
+    case RendererError::BoardTooSmall:
+      return "Reported a min. supported pixel length less than 1";
+    case RendererError::BoardBadRange:
+      return "Reported a max. supported pixel length less than the min.";
+    case RendererError::GameDimensionBad:
+      return "Game dimension is not supported";
+    case RendererError::BoardNotSquare:
+      return "Game rect. must be square";
+    case RendererError::BoardSizeBad:
+      return "Game rect. size not supported by this renderer";
+    case RendererError::BadRegion:
+      return "Negative height and width values are invalid";
+    case RendererError::CellCoordinateBad:
+      return "Negative board cell X and Y index values are invalid";
+    case RendererError::NewStateInvalid:
+      return "Requested new board cell state value is invalid";
+    case RendererError::PrevStateInvalid:
+      return "Claimed previous state value is invalid";
     default:
       return "?? unrecognised error ??";
     }
   }
 
-  const RendererPanicCategory theRendererPanicCategoryObj {};
+  const RendererErrorCategory theRendererErrorCategoryObj {};
 
-  struct AppEnginePanicCategory : std::error_category
+  struct AppEngineErrorCategory : std::error_category
   {
     const char* name() const noexcept override;
     std::string message(int ev) const override;
   };
 
-  const char* AppEnginePanicCategory::name() const noexcept 
+  const char* AppEngineErrorCategory::name() const noexcept 
   {
     return "app-engine";
   }
 
-  std::string AppEnginePanicCategory::message( int ev ) const
+  std::string AppEngineErrorCategory::message( int ev ) const
   {
-    using TheGame::AppEnginePanic;
-    switch( static_cast<AppEnginePanic>(ev) )
+    using TheGame::AppEngineError;
+    switch( static_cast<AppEngineError>(ev) )
     {
-    case AppEnginePanic::NoObjectConIx:
+    case AppEngineError::NoObjectConIx:
       return "No object container index to create object container from";
-    case AppEnginePanic::NoRendererContainer:
+    case AppEngineError::NoRendererContainer:
       return "No renderer container to access renderers through";
-    case AppEnginePanic::NullDrawAction:
+    case AppEngineError::NullDrawAction:
       return "Null draw action pointer";
-    case AppEnginePanic::BadDrawContext:
-      return "Draw action context has NULL graphics context or renderer pointer";
-    case AppEnginePanic::BadGameObject:
-      return "Draw action context has NULL game object pointer";
-    case AppEnginePanic::NullPlayer:
+    case AppEngineError::BadDrawContext:
+      return "Draw action context has null graphics context or renderer pointer";
+    case AppEngineError::BadGameObject:
+      return "Draw action context has null game object pointer";
+    case AppEngineError::NullPlayer:
       return "Current player pointer is null";
     default:
       return "?? unrecognised error ??";
     }
   }
 
-  const AppEnginePanicCategory theAppEnginePanicCategoryObj {};
+  const AppEngineErrorCategory theAppEngineErrorCategoryObj {};
 }
 
 namespace TheGame
 {
-  std::error_code make_error_code(RendererPanic e)
+  std::error_code make_error_code(RendererError e)
   {
-    return {static_cast<int>(e), theRendererPanicCategoryObj};
+    return {static_cast<int>(e), theRendererErrorCategoryObj};
   }
 
-  std::error_code make_error_code(AppEnginePanic e)
+  std::error_code make_error_code(AppEngineError e)
   {
-    return {static_cast<int>(e), theAppEnginePanicCategoryObj};
+    return {static_cast<int>(e), theAppEngineErrorCategoryObj};
   }
 }
 
@@ -157,8 +155,8 @@ namespace TheGame
 
 int main()
 {
-  std::error_code r_ec{ TheGame::RendererPanic::BoardNotSquare };
-  std::error_code ae_ec{ TheGame::AppEnginePanic::BadDrawContext };
+  std::error_code r_ec{ TheGame::RendererError::BoardNotSquare };
+  std::error_code ae_ec{ TheGame::AppEngineError::BadDrawContext };
   std::cout << r_ec << " " << r_ec.message() << "\n" 
             << ae_ec << " " << ae_ec.message() << std::endl;
 }
